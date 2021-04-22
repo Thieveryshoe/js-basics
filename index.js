ac35598dfed3e4f6935e929b03facd7b53090a89
John Ellison <jellison@ShoeBox13.local>
2021-04-22 15:56:30 -0500

    exercise - swapping variables - solved

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let a = 'red';
 let b = 'blue';
+let cache = a;
+a = b;
+b = cache;
 
-console.log(a); // red
-console.log(b); // blue
+console.log(a); // red => blue
+console.log(b); // blue => red
 
 // write code to swap the color values
