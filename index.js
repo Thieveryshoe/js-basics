9110930e9cb9a486a722592bf4ef9378971bb89b
John Ellison <jellison@ShoeBox13.local>
2021-04-22 14:37:02 -0500

    ternary operator -- works as expected

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
-let x = 1;
+// If a customer has more than 100 points,
+// they are a gold customer.  otherwise,
+// they are a silver customer.
 
-// relational operators
-console.log(x > 0);
-console.log(x >= 1);
-console.log(x < 1);
-console.log(x <= 1);
+let points = 110;
 
-// equality operators
-console.log(x === 1);
-console.log(x !== 1);
+console.log(getType(points)); // gold
+console.log('points value reduced below gold threshold');
+points = 50;
+console.log(getType(points)); // silver
 
-console.log('Begin strict v loose:');
-// strict equality
-// value and type are the same
-console.log(1 === 1); // true
-console.log('1' === 1); // false
-// loose equality
-// value only is the same
-console.log(1 == 1); // true
-console.log('1' == 1); //true
-// transform looks like: '1' == 1 =>  '1' == '1'
-console.log('Begin comparing booleans:');
-console.log(true == 1); // true
-console.log(true == 0); // false
-console.log(true === 1); // false
-
-// most of the time, just use strict until given a reason not to.
-// and try not to give yourself a reason either.
-// and just set your linter to always convert to strict.
+function getType(points) {
+    return points > 100 ? 'gold' : 'silver';
+}
