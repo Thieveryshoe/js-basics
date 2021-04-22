52ef55d2d5267b38dd2d86cce38024b9c0f794f4
John Ellison <jellison@ShoeBox13.local>
2021-04-22 11:45:31 -0500

    more function notes -- course section complete

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ function great(firstName, lastName) {
     console.log(`hello ${firstName} ${lastName}`); //template string - string literals work
 }
 
-great('John', 'Ellison');
-great('Leslie', 'Ward');
+// great('John', 'Ellison');
+// great('Leslie', 'Ward');
+
+
+function square(number) {
+    return number * number;
+}
+// let number = square(2);
+console.log(square(2));
