b7502be2a52e40562283b858337532fc0b7a7c47
John Ellison <jellison@ShoeBox13.local>
2021-04-22 14:11:33 -0500

    comparison and equality operators
    comparison and equality operators

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-let x = 10;
-console.log(x += 5); // 15
-console.log(x *= 2); // 30
-console.log(x /= 10); // 3
-console.log(x %= 2) // 1
-console.log(x -= 1); // 0
+let x = 1;
+
+// relational operators
+console.log(x > 0);
+console.log(x >= 1);
+console.log(x < 1);
+console.log(x <= 1);
+
+// equality operators
+console.log(x === 1);
+console.log(x !== 1);
+
+console.log('Begin strict v loose:');
+// strict equality
+// value and type are the same
+console.log(1 === 1); // true
+console.log('1' === 1); // false
+// loose equality
+// value only is the same
+console.log(1 == 1); // true
+console.log('1' == 1); //true
+// transform looks like: '1' == 1 =>  '1' == '1'
+console.log('Begin comparing booleans:');
+console.log(true == 1); // true
+console.log(true == 0); // false
+console.log(true === 1); // false
+
+// most of the time, just use strict until given a reason not to.
+// and try not to give yourself a reason either.
+// and just set your linter to always convert to strict.
