7613278e93ce6803f81f20741ce68debaeccc4fa
John Ellison <jellison@ShoeBox13.local>
2021-04-22 11:06:51 -0500

    reference type: object

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+// reference type => object
+let person1 = {
+    name: 'john',
+    age: 36
+};
+console.log(person1);
+// dot notation
+console.log(person1.name);
+
+// bracket notation => can be used at run time
+let person2 = {
+    name: 'Leslie',
+    age: 33
+};
+console.log(person2['name']);
+console.log(person2['age']);
