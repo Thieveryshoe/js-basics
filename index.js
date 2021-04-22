503ed5c07b535d4b921e338911023dc1da642260
John Ellison <jellison@ShoeBox13.local>
2021-04-22 14:43:57 -0500

    logical operators -- and, or, not
    logical operators -- and, or, not

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-// If a customer has more than 100 points,
-// they are a gold customer.  otherwise,
-// they are a silver customer.
-
-let points = 110;
-
-console.log(getType(points)); // gold
-console.log('points value reduced below gold threshold');
-points = 50;
-console.log(getType(points)); // silver
-
-function getType(points) {
-    return points > 100 ? 'gold' : 'silver';
-}
+// all of these logical operators function as expected
+// when working with booleans
+//  && (AND)
+// || (OR)
+// ! (BANG)
