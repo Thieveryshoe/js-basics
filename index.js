7d054b10fa576d3d34eebb3452c42f7c877d3521
John Ellison <jellison@ShoeBox13.local>
2021-04-22 13:56:38 -0500

    assignemnt operator notes

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-// + - * / % are the same as expected
-// ** is special to the power of operator
-//  x ** y => x to the power of y
-
-let number = 10;
-console.log(number);  // 10
-console.log(number++); // 10
-console.log(number); // 11
-console.log(++number); //12
-console.log(number); // 12
-
-
-console.log('start decrements');
-console.log(--number); // 11
-console.log(--number); // 10
-console.log(number--); // 10
-console.log(number); // 9
+let x = 10;
+console.log(x += 5); // 15
+console.log(x *= 2); // 30
+console.log(x /= 10); // 3
+console.log(x %= 2) // 1
+console.log(x -= 1); // 0
