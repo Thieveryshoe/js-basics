c613eade89acf2288b83be025c2468643bf476b9
John Ellison <jellison@ShoeBox13.local>
2021-04-23 11:15:46 -0500

    control flow - for-of loop

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // for-in loops
+// use for in to iterate over propeties of an object
 let person = {
     name: 'John',
     age: 36
@@ -16,3 +17,15 @@ for (let index in colors) {
     console.log(index); // prints current value of index in the array
     console.log(colors[index]); // prints current value of the index
 }
+
+// for-of lop => for-each
+console.log('begin for-of');
+const shapes = ['triangle', 'square', 'circle'];
+for (let c of shapes) {
+    console.log(c);
+}
+
+// 
+console.log('begin extension method');
+shapes.forEach(x => console.log(x));
+// use for of to iterate over items in an array
