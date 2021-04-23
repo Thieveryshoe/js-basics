adbbf0705d17a82ae7b1e7efee219b10782faaa0
John Ellison <jellison@ShoeBox13.local>
2021-04-23 10:48:13 -0500

    control flow - for, while, do-while loops

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-let role;
-// role = 'guest';
-role = 'admin';
-
-switch (role) {
-    case 'guest':
-        console.log('guest user');
-        break;
-    case 'admin':
-        console.log('admin user');
-        break;
-    default:
-        console.log('unknown user');
-        break;
+// for loop, works as expected
+console.log('Begin for loop');
+for (let i = 0; i < 3; i++) {
+    console.log('Hello world.', i);
 }
+
+// while loop, works as expected
+console.log('Begin while loop');
+let i = 4;
+while (i < 3) {
+    console.log('Hello world.', i);
+    i++;
+}
+
+// do-while loop, works as expected, always executed at least once.
+console.log('Begin do-while loop');
+i = 0;
+do {
+    console.log('Hello world.', i);
+    i++;
+} while (i < 3);
+
