b3e0498d7d3e6a599e823590565440643910b75c
John Ellison <jellison@ShoeBox13.local>
2021-04-23 10:33:20 -0500

    control flow - switch and case

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-let hour = 10;
-console.log(getGreeting(hour)); // Good morning
-hour = 15;
-console.log(getGreeting(hour)); // Good afternoon
-hour = 22;
-console.log(getGreeting(hour)); // Good evening
+let role;
+// role = 'guest';
+role = 'admin';
 
-function getGreeting(hour) {
-    if (hour > 6 && hour < 12) {
-        return 'Good morning';
-    }
-    // else if (hour > 12 && hour < 18) {
-    //     return 'Good afternoon';
-    // }
-    // brackets for multi line code blocs
-    else if (hour > 12 && hour < 18)
-        return 'Good afternoon';
-    else
-        return 'Good evening'
+switch (role) {
+    case 'guest':
+        console.log('guest user');
+        break;
+    case 'admin':
+        console.log('admin user');
+        break;
+    default:
+        console.log('unknown user');
+        break;
 }
