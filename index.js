416d39a5c1b5fbcd4b385551b59086b38257d13f
John Ellison <jellison@ShoeBox13.local>
2021-04-25 09:32:08 -0500

    object basics

--- index.js
+++ index.js
@@ -1,42 +1,14 @@
-// fizzBuzz
-// if argument is divisible by 3 => fizz
-// if argument is divisible by 5 => buzz
-// if argument is divisible by 3 && 5 => fizzbuzz
-// if argument is not divisible by 3 || 5 => argument
-// if argument is not a number => 'not a number'
-
-console.log(fizzBuzz(3)); // fizz
-console.log(fizzBuzz(5)); // buzz
-console.log(fizzBuzz(15)); // fizzbuzz
-console.log(fizzBuzz(7)); // 7 
-console.log(fizzBuzz('sadf')); // not a number
-
-function fizzBuzz(num) {
-    if (typeof(num) !== 'number') {
-        return NaN;
+// OOP style.  Basics are as expected.
+const circle = {
+    radisu: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    isVisible: true,
+    draw: function() {
+        console.log('draw');
     }
+};
 
-    let divByThree = false;
-    let divByFive = false;
-
-    if (num % 3 === 0) {
-        divByThree = true;
-    }
-
-    if (num % 5 === 0) {
-        divByFive = true;
-    }
-
-    if (divByThree && !divByFive) {
-        return 'fizz';
-    }
-    if (!divByThree && divByFive) {
-        return 'buzz';
-    }
-    if (divByThree && divByFive) {
-        return 'fizzbuzz'
-    }
-    return num;
-}
-
-// test commit
+circle.draw();
