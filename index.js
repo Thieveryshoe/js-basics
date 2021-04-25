d94fd4c494574a0d34f0b2ccc7ba91741c65f57e
John Ellison <jellison@ShoeBox13.local>
2021-04-25 09:52:25 -0500

    dynamic nature of objects

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
-// OOP style.  Basics are as expected.
-
-// Factory function
-function createCircle(radius) {
-    return {
-        radius, // if key and value are the same, can remove the value. radius: radius => radius
-        draw() { // can drop the function keyword in the signature
-            console.log('draw');
-        }
-    };
+// dynamic nature of objects in js
+// const here means we cannot reassign the variable, not the properties of the variable
+const circle = {
+    radius: 1
 }
 
-const circle1 = createCircle(1);
-console.log(circle1);
-const circle2 = createCircle(13);
-console.log(circle2);
-
-console.log('begin constructor functions');
-// constructor functions
-// pascal case for constructor functions
-
-function Circle(radius) {
-    this.radius = radius;
-    this.draw = function() {
-        console.log('draw using constructor function');
-    }
-}
-
-const circle = new Circle(34);
 console.log(circle);
-circle.draw();
+
+circle.color = 'yellow'
+
+console.log(circle);
+
+delete circle.color;
+
+console.log(circle);
