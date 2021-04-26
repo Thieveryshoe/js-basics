e49734d7e548882143bef938b619e2dff0023163
John Ellison <jellison@ShoeBox13.local>
2021-04-26 11:45:58 -0500

    arrays: joining and splitting

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const numbers = [1,2,3];
 
-for (let n of numbers)
-    console.log(n);
+const joined = numbers.join('|'); // returns a string
+console.log(joined);
 
-// awww sweet syntax sugar
-numbers.forEach(n => console.log(n));
-
-numbers.forEach((n, index) => console.log(n, index));
+const parts = joined.split('|') // returns an array
+console.log(parts);
