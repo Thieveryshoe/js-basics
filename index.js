b2f9e828d98e903efbf2649d4e5205554a9f37cd
John Ellison <jellison@ShoeBox13.local>
2021-04-26 10:31:10 -0500

    arrays: locating index of primitives

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const numbers = [3, 4];
-console.log(numbers);
-// add to end
-numbers.push(5, 6);
-console.log(numbers);
-// add to beginning
-numbers.unshift(1, 2);
-console.log(numbers);
-// add to middle
-numbers.splice(2, 0, 2.5);
-console.log(numbers);
+// finding primitives
+const numbers = [1, 2, 3, 4];
+// find 2
+console.log(numbers.indexOf(2)); // 1
+// find 6
+console.log(numbers.indexOf(6)); // -1 => does not exist
+// find last index of 1
+numbers.push(1);
+console.log(numbers.lastIndexOf(1)); // 4
+// does number include argument?
+console.log(numbers.includes(4)); // true
