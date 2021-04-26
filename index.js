7459eb0724f10427211ed0e0212e4df0bf634c3c
John Ellison <jellison@ShoeBox13.local>
2021-04-26 12:09:53 -0500

    arrays: filtering

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const numbers = [1,2,3];
+const numbers = [1, -2, 2, 3];
+const positiveNumbers = numbers.filter(x => x >= 0);
+console.log(positiveNumbers);
 
-const allPositive = numbers.every(x => x >= 0);
-console.log(allPositive);
+const stores = [
+    { Id: 1, isOpen: true},
+    { Id: 1, isOpen: false}
+];
+console.log('all stores', stores);
+const openStores = stores.filter(x => x.isOpen === true);
+console.log('open stores', openStores);
 
-const numbers2 = [1,-2,3];
-
-const allPositive2 = numbers2.every(x => x >= 0);
-console.log(allPositive2);
-
-const numbers3 = [1, -2, 2, 3];
-const atLeastOneNegative = numbers3.some(x => x < 0);
-console.log(atLeastOneNegative);
