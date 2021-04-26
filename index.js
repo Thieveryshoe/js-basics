9c0cb059182cb7392fba6770c0454adf06aac56c
John Ellison <jellison@ShoeBox13.local>
2021-04-26 10:59:08 -0500

    arrays: removing elements

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-// finding reference types
-const courses = [
-    { id: 1, name: 'a'},
-    { id: 2, name: 'b'}
-];
+const numbers = [1,2,3,4,5]
 
-console.log(courses.includes({ id: 1, name: 'a'})); // False
-
-let found = courses.find(c => c.name === 'a');
-
-console.log(found); // logs the object
-
-let found2 = courses.findIndex(c => c.name === 'a');
-
-console.log(found2); // 0
+console.log(numbers);
+// remove from end
+let last = numbers.pop();
+console.log(last);
+console.log(numbers);
+// remove from beginning
+let first = numbers.shift()
+console.log(first);
+console.log(numbers);
+// remove from middle
+let middle = numbers.splice(1, 1);
+console.log(middle);
+console.log(numbers);
