d8284a24504fe64f88821b69364bb77486c8b73c
John Ellison <jellison@ShoeBox13.local>
2021-04-26 09:32:31 -0500

    exercise: blog post object

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
-let address1 = new Address('a', 'b', 'c');
-let address2 = new Address('a', 'b', 'c');
-let address3 = address1;
-
-console.log(propertiesAreEqual(address1, address2)); //true
-console.log(isSameObject(address1, address2)); // false
-console.log(isSameObject(address1, address3)); // true
-
-// Constructor function
-function Address(street, city, zipCode) {
-    this.street = street;
-    this.city = city,
-    this.zipCode = zipCode;
-}
-
-function propertiesAreEqual(a, b) {
-    return a.street === b.street
-        && a.city === b.city
-        && a.zipCode === b.zipCode
-}
-
-function isSameObject(a, b) {
-    return a === b; // checks the memory reference
-}
+const blogPost = {
+    title: 'test title',
+    body: 'test body',
+    author: 'test authod',
+    views: 0,
+    isLive: false,
+    comments: [
+        {
+            author: 'test commentor',
+            body: 'test comment body'
+        },
+        {
+            author: '2',
+            body: '2 body'
+        }
+    ]
+};
