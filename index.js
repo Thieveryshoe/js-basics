eb749d0246b87c898a4e98b8d85a46fe2ad75112
John Ellison <jellison@ShoeBox13.local>
2021-04-26 09:02:59 -0500

    exercise: address object

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-// the js engine has a garbage collector for free.
-// we do not have to worry about allocating and deallocating memory
-// locations.  it's handled automatically.
+const address = {
+    street: 'Odell',
+    city: 'St. Louis',
+    zipCode: '63139'
+};
+
+
+function showAddress(address) {
+    for (let key in address) {
+        console.log(`${key} is ${address[key]}`);
+    }
+}
+
+showAddress(address);
