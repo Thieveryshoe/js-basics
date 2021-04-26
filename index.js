bcf9a80769f6026446d41c491fe85af9124b3a69
John Ellison <jellison@ShoeBox13.local>
2021-04-26 16:13:53 -0500

    Dynamic Arguments are 'fun'

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-// function declaration
-function walk() {
-    console.log('walk');
+// js is a dynamic languate
+
+function sum(a, b) {
+    return a + b;
 }
 
-// anonymous function expression
-const run = function() {
-    console.log('run');
-};
+console.log(sum(1, 2)); // 3
+console.log(sum(1)); // NaN
+console.log(sum()); // NaN
+console.log(sum('foo', 'Fighters')); //fooFighters
+console.log(sum(1, 'Two')); //1Two
+console.log(sum(1, 2, 3, 4)); // 3
 
-// named function expression
-const jog = function jog() {
-    console.log('jog');
-};
-
-let move = run;
-
-walk(); // walk
-run(); // run
-jog(); // jog
-move(); // run
+// every function has a special property called arguments
+function times() {
+    total = 1;
+    Array.from(arguments).forEach(a => total *= a);
+    return total;
+}
+console.log('begin times:');
+console.log(times(2)); // 2
+console.log(times(1, 2, 3)); // 6
+console.log(times(10, 44, 9, 0)); // 0
