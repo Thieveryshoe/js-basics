01abe83fc338cd76c7c733df77dce95a2d1fdfc8
John Ellison <jellison@ShoeBox13.local>
2021-04-26 11:39:28 -0500

    arrays: iterating over an array

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-const first = [1,2,3];
-const second = [4,5,6];
+const numbers = [1,2,3];
 
-const combined = [...first, ...second];
-console.log('combined', combined);
-const combined2 = first.concat(second)
-console.log('combined2', combined2);
+for (let n of numbers)
+    console.log(n);
 
-const combined3 = [...first, ...second, 'b'];
-console.log('combined3', combined3);
+// awww sweet syntax sugar
+numbers.forEach(n => console.log(n));
 
-const copy = combined3.slice();
-console.log('copy1', copy);
-
-const copy2 = [...combined3];
-console.log('copy2', copy2);
+numbers.forEach((n, index) => console.log(n, index));
