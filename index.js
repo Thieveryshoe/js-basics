15c86d18e0da2d81a73e98354a79a4c23cf20cbc
John Ellison <jellison@ShoeBox13.local>
2021-04-27 08:58:32 -0500

    this keyword

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
-// using let, variable is limited to the code block it's defined in => scope is the loop in this case
-function startLet() {
-    console.log('logging using let:');
-    for (let i = 0; i < 5; i++)
-        console.log(i);
-    // console.log(i); // uncaught reference: i is not defined error when trying to use htis line
+// what is this?
+// this is the object that is executing the current function
+
+// method (a function that is part of an object) => this represents that object
+// function (function not part of an object) => this represents the global object (window in browser, global in node)
+
+const video = {
+    title: 'a',
+    play() {
+        console.log(this);
+    }
+};
+
+video.stop = function() {
+    console.log(this);
+};
+
+// both will log the video obj, the method concept of this
+video.play();
+video.stop();
+
+function playVideo() {
+    console.log(this);
 }
 
-// using let, variable is limited to the function it's defined in => scope is the function in this case
-function startVar() {
-    console.log('logging using var:');
-    for (var i = 0; i < 5; i++)
-        console.log(i);   
-    console.log(i);
-    console.log(i);
+// will log the window obj, the global concept of this
+playVideo();
+
+function GetVideo(title) {
+    this.title = title;
+    console.log(this);
 }
 
-// var => function-scoped variabls, was the only way to define variables for awhile
-// ES6 (ES2015): let, const added => create block-scoped variables
+// will be using the method concept as the js engine creates a new {} when using new keyword
+const v = new GetVideo('b');
 
-startLet();
-startVar();
+const album = {
+    title: 'Ten',
+    tags: ['alternative', '90s', 'grunge'],
+    showTags() {
+        this.tags.forEach(t => console.log(this.title, t));
+    }
+};
+
+album.showTags();
