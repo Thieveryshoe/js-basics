b3c2502db3e648b6f6ba0fdc63776b572a029fc5
John Ellison <jellison@ShoeBox13.local>
2021-04-27 08:40:07 -0500

    let vs var

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-const person = {
-    firstName: 'John',
-    lastName: 'Ellison',
-    get fullName() {
-        return `${person.firstName} ${person.lastName}`;
-    },
-    set fullName(fullName) {
-        // if (typeof fullName !== 'string') return; // this is one way to be defensive
-        if (typeof fullName !== 'string')
-            throw new Error('Value is not a string.');
-        const parts = fullName.split(' ');
-        if (parts.length !== 2)
-            throw new Error('Enter only first and last name.')
-        this.firstName = parts[0];
-        this.lastName = parts[1];
-    }
-};
-
-try {
-    // person.fullName = null;
-    person.fullName = 'John sdf dfljd';
-} catch (e) {
-    alert(e);
+// using let, variable is limited to the code block it's defined in => scope is the loop in this case
+function startLet() {
+    console.log('logging using let:');
+    for (let i = 0; i < 5; i++)
+        console.log(i);
+    // console.log(i); // uncaught reference: i is not defined error when trying to use htis line
 }
-// console.log(person); // fullName is null error
+
+// using let, variable is limited to the function it's defined in => scope is the function in this case
+function startVar() {
+    console.log('logging using var:');
+    for (var i = 0; i < 5; i++)
+        console.log(i);   
+    console.log(i);
+    console.log(i);
+}
+
+// var => function-scoped variabls, was the only way to define variables for awhile
+// ES6 (ES2015): let, const added => create block-scoped variables
+
+startLet();
+startVar();
