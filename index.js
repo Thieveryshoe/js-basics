46e03198f6938da28a886a316e026045fd5835b7
John Ellison <jellison@ShoeBox13.local>
2021-04-27 07:36:38 -0500

    Default Parameters

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-// when we apply the rest operator to a parameter of a function
-// we can pass varying number of arguments and put them into an array
-// rest parameter must be the last formal parameter.
-// funciton sum(discount, ...args, soemthingElse) => won't work
-function sum(...args) {
-    return args.reduce((a, b) => a + b);
-}
-console.log('sum:');
-console.log(sum(1,2,3,4,5,6));
+// default  paramets
+// function interest(principal, rate, years) {
+//     return principal * rate / 100 * years;
+// }
+// what if we want default values for interest rate and years
 
-function getTotal(discount, ...prices) {
-    let total = 0;
-    prices.forEach(p => total += p);
-    return total * (1 - discount);
+function interest(principal, rate, years) {
+    rate = rate || 3.5; // if rate is truthy, use that, else 3.5
+    years = years || 5;   // if years is truthy, use that, else 30
+    return principal * rate / 100 * years;
 }
-console.log('getTotal');
-console.log(getTotal(.1, 100, 50));
+
+console.log('given all params', interest(10000, 3.5, 5));
+console.log('given only amount', interest(10000));
+
+// can write in even more modern way
+function calculateInterest(principal, rate = 3.5, years = 5) {
+    return principal * rate / 100 * years;
+}
+console.log('calculateInterest with all pararms', calculateInterest(10000, 3.5, 5));
+console.log('calculateInterest with only amount param', calculateInterest(10000));
